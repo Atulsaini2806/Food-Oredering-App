38b3aa1bb4f44bba17f49e3cc94cab6ea609540c
Atulsaini2806 <atulsaini10123@gamil.com>
2026-01-07 17:17:39 +0530

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+
+const Footer=()=>{
+    return(
+        <footer className="bg-gray-800 text-white text-center py-7 mt-auto">
+            <p>&copy; {new Date().getFullYear()} Food Ordering App. All rights reserved.</p>
+        </footer>
+    )
+}
+export default Footer;
