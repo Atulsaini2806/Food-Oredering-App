1917afa8be434029a793a19887f67af9d4bfd430
Atulsaini2806 <atulsaini10123@gamil.com>
2026-01-07 17:17:29 +0530

    Contact Us

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,10 +1,15 @@
 const ContactUs = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <h2>This is the contact us page.</h2>
-    </div>
+  
+  <div className="flex flex-col items-center ">
+            <h1 className="font-bold text-3xl my-5">This is Contact Page!</h1>
+            <form className="flex flex-col w-[300px] h-[400px]  border-black border-2 p-2 m-2 rounded-2xl">
+                <input type="text" className="border border-black p-2 m-2 rounded-lg" placeholder="name"/>
+                <input type="text" className="border border-black p-2 m-2 rounded-lg" placeholder="message"/>
+                <button className="bg-blue-300 border rounded-lg p-2 m-2">Submit</button>
+            </form>
+        </div>
   );
-};
+}
 
 export default ContactUs;
