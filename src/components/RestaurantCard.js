c6cf4c6f1098e3d14a2e4aec322a39f7fd42b8dc
Atulsaini2806 <atulsaini10123@gamil.com>
2026-01-07 21:18:18 +0530

    Restaurant Card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,21 +8,7 @@ const RestaurantCard = ({ resData }) => {
 
   const { name, cuisines, avgRating, costForTwo,sla, cloudinaryImageId } = resData.info;
 
-  // return (
-  //   <div className="m-3 p-3 w-[200px] h-[450px] rounded-lg bg-gray-200 hover:bg-gray-300">
-  //     <img
-  //       className="rounded-lg "
-  //       alt={name}
-  //       src={`${CDN_URL}${cloudinaryImageId}`} 
-  //     />
-  //     <h4 className="font-bold py-3 text-sm">{name}</h4>
-  //     <h4>{cuisines.join(", ")}</h4>
-  //     <h4>{avgRating} ⭐</h4>
-  //     <h4>{costForTwo}</h4>
-  //     <h4>{sla.deliveryTime} mins</h4>
-  //      <h4>User : {loggedInUser} </h4> 
-  //   </div>
-  // );
+  
   return (
     <div className="m-3 p-3 w-[200px] h-[320px] rounded-lg bg-gray-200 hover:bg-gray-300 flex flex-col">
       
